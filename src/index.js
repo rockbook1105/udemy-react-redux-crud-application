727f86a90314818cfa6cfec08a998c66fd10efc7
rockbook1105 <rockbook1105.xxx@gmail.com>
2018-09-29 00:27:02 +0900

    implement delete feature

--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
+import { composeWithDevTools } from 'redux-devtools-extension'
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new'
+import EventsShow from './components/events_show';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-      
-        <Route exact path="/events/new" component={EventsNew} />
 
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/events/:id" component={EventsShow} />
         <Route exact path="/" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
